c2ce1b8ee8a9c094618e2c957381d28f22e0e31e
samir <samir.m8@gmail.com>
2024-09-26 15:59:05 -0400

    refactoring, adding eb setup

--- norbit/norbit/asgi.py
+++ norbit/norbit/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for test_norb project.
+ASGI config for norbit project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_norb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'norbit.settings')
 
 application = get_asgi_application()
--- norbit/norbit/wsgi.py
+++ norbit/norbit/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for test_norb project.
+WSGI config for norbit project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_norb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'norbit.settings')
 
 application = get_wsgi_application()
