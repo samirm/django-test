81c298327d61964ee676c86d89a76ab180bfdd3e
samir <samir.m8@gmail.com>
2024-09-26 16:29:14 -0400

    refactoring to use postgres

--- norbit/norbit/settings.py
+++ norbit/norbit/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = 'norbit.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    "postgres": {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "test-db",
-        "USER": "user",
-        "PASSWORD": "myPassword",
-        "HOST": "127.0.0.1",
+        "NAME": "template1",
+        "USER": "postgres",
+        "PASSWORD": "passyPass",
+        "HOST": "localhost",
         "PORT": "5432",
     }
 }
